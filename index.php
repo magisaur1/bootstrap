02ad0ac7088cb6acf1652453d4e3393e5b2a37c0
magisaur1 <96684606+magisaur1@users.noreply.github.com>
2021-12-26 18:58:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,5 +10,42 @@
 	<h1>Cara Menggunakan Bootstrap | www.malasngoding.com</h1>
 	<button class="btn btn-danger">TOMBOL MERAH</button>
 	<button class="btn btn-primary">TOMBOL BIRU</button>
+	<h1>Cara Membuat table dengan Bootstrap | www.malasngoding.com</h1>
+	<table class="table">
+		<thead>
+			<tr>
+				<th>No</th>
+				<th>Makanan</th>
+				<th>Harga</th>				
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>Bakso</td>
+				<td>12.000</td>
+			</tr>
+			<tr>
+				<td>2</td>
+				<td>Mie Goreng</td>
+				<td>7.000</td>
+			</tr>
+			<tr>
+				<td>3</td>
+				<td>Nasi Goreng</td>
+				<td>15.000</td>
+			</tr>
+			<tr>
+				<td>4</td>
+				<td>Sate Padang</td>
+				<td>17.000</td>
+			</tr>
+			<tr>
+				<td>5</td>
+				<td>Nasi Soto</td>
+				<td>20.000</td>
+			</tr>
+		</tbody>
+	</table>
 </body>
 </html>
